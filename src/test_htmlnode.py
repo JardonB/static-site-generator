fb9eb61f0f10d73e7ccb243743096abd8ace2e63
JardonB <jordan.breitling@gmail.com>
2025-10-25 15:38:48 -0600

    created HTMLNode class and related unit tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html_empty_node(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), '')
+    
+    def test_props_to_html_one_prop(self):
+        node = HTMLNode(props={
+            "href": "https://www.google.com",
+        })
+        self.assertEqual(node.props_to_html(), ' href="https://www.google.com"')
+    
+    def test_props_to_html(self):
+        node = HTMLNode(props={
+            "href": "https://www.google.com",
+            "target": "_blank",
+        })
+        self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+    
+
+if __name__ == "__main__":
+    unittest.main()
