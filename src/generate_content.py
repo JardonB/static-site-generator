4a10dac0efa30b0e770c501fdaacee87bfd56b75
JardonB <jordan.breitling@gmail.com>
2025-10-30 19:28:07 -0600

    Added base_path variable and updated link sources to use the base_path

--- src/generate_content.py
+++ src/generate_content.py
@@ -8,7 +8,7 @@ logging.basicConfig(
     filemode="w"
 )
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     logging.info(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown, template = "", ""
 
@@ -20,7 +20,10 @@ def generate_page(from_path, template_path, dest_path):
     inner_html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
 
-    html = template.replace("{{ Content }}", inner_html).replace("{{ Title }}", title)
+    html = template.replace("{{ Content }}", inner_html)    #Fill content with inner_html
+    html = html.replace("{{ Title }}", title)               #Replace title with extracted title
+    html = html.replace('href="/', f'href="{base_path}')    #Replace href path with base_path
+    html = html.replace('src="/', f'src="{base_path}')      #Replace src path with base_path
 
     if not os.path.exists(dest_path):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -28,7 +31,7 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     contents = os.listdir(dir_path_content)
     for item in contents:
         file_path = os.path.join(dir_path_content, item)
@@ -36,9 +39,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(file_path):
             if file_path.endswith(".md"):
                 dest_path = os.path.join(dest_dir_path, (f"{item.split(".")[0]}.html"))
-                generate_page(file_path, template_path, dest_path)
+                generate_page(file_path, template_path, dest_path, base_path)
         else:
-            generate_pages_recursive(file_path, template_path, dest_path)
+            generate_pages_recursive(file_path, template_path, dest_path, base_path)
     
 def extract_title(markdown):
     md_lines = markdown.splitlines()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
+import sys
 from copy_static import copy_static
 from generate_content import generate_pages_recursive
 
-public_path = "./public/"
-static_path = "./static/"
+dest_path = "docs"
+static_path = "static"
+content_path = "content"
+template_path = "template.html" 
+base_path = "/"
+
+if sys.argv[0]:
+    base_path = sys.argv[0]
 
 def main():
-    copy_static(static_path, public_path)
-    generate_pages_recursive("./content", "./template.html", "./public")
+    copy_static(static_path, dest_path)
+    generate_pages_recursive(content_path, template_path, dest_path, base_path)
 
 
 if __name__ == "__main__":
