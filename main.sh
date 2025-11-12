8875e1b824e8a01ee4d4f0a31c1351927c255f80
JardonB <62777896+JardonB@users.noreply.github.com>
2025-11-12 16:10:40 -0700

    Changed http server to run on port defined by a variable

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
-#Runs main.py and opens http server on port 8888
+#Port that the http server will run on
+PORT=8888
+
+#Runs main.py and opens http server on port
 python3 src/main.py --verbose
-cd docs && python3 -m http.server 8888
+cd docs && python3 -m http.server $PORT
