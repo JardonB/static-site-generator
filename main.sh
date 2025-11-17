a90d43d0500186eeb934d1de8f99091cceb4047c
JardonB <jordan.breitling@gmail.com>
2025-11-16 18:19:05 -0700

    More reliable sys.argv handling and added basepath arg and variable

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #Port that the http server will run on
 PORT=8888
+BASEPATH="/"
 
-#Runs main.py and opens http server on port
-python3 src/main.py --verbose
+#Runs main.py and opens http server on $PORT
+python3 src/main.py $BASEPATH --verbose
 cd docs && python3 -m http.server $PORT
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from copy_static import copy_static
 from generate_content import generate_pages_recursive, extract_template_indents
 from htmlnode import leading_spaces
@@ -7,25 +8,25 @@ from htmlnode import leading_spaces
 dest_path = "docs"
 static_path = "static"
 content_path = "content"
-template_path = "template.html" 
-base_path = "/"
+template_path = "template.html"
 
 # Indent settings
 template_indents = extract_template_indents(template_path)
 
-verbose = False
-
-if len(sys.argv) >= 2:
-    base_path = sys.argv[1]
-    if "--verbose" in sys.argv:
-        verbose = True
-
-def main():
+def main(basepath="/", verbose=False):
     if verbose:
+        print(f"Base path: {basepath}")
         print(f"template indents\n\tspaces per indent: {template_indents[0]}\n\tbase content indents: {template_indents[1]}\n\tLeading spaces: \"{leading_spaces(template_indents)}\"\n\tnum of leading spaces: {len(leading_spaces(template_indents))}")
     copy_static(static_path, dest_path, verbose=verbose)
-    generate_pages_recursive(content_path, template_path, dest_path, base_path, template_indents, verbose=verbose)
+    generate_pages_recursive(content_path, template_path, dest_path, base_path=basepath, indents=template_indents, verbose=verbose)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog="Static site generator",
+                                    description="Generates a static website from content, static, and template files.",
+                                    epilog="Example usage: python src/main.py /base/path -v")
+    parser.add_argument("basepath", help="base path for the website")
+    parser.add_argument("-v", "--verbose", action="store_true", help="print verbose output")
+    args = parser.parse_args()
+
+    main(basepath=args.basepath, verbose=args.verbose)
